d4269961c3db39c1973fede0196d47e67fb70f86
David Wong <david@wo.ng>
2022-10-26 17:48:57 +0100

    Pluralised word test

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
+const stripPlural = (word) => {
+  const pluralisingTokens = /(s\b|\b)/g
+  const foundPlural = word.match(pluralisingTokens).filter(x => x !=='').join('')
+  return word.replace(foundPlural, '')
+}
 const stemWord = (word) => {
-  return word
+  return stripPlural(word)
 }
 
 module.exports = stemWord
--- src/index.spec.js
+++ src/index.spec.js
@@ -1,4 +1,4 @@
-const stemWord = require('./index.js').default
+const stemWord = require('./index.js')
 
 describe('Stemmer', () => {
   describe('Given a word that cannot be stemmed', () => {
@@ -6,6 +6,23 @@ describe('Stemmer', () => {
       const originalWord = 'following' 
       const result = stemWord(originalWord)
       expect(result).toEqual('following')
+    })
+  })
+  describe('Given a pluralised word', () => {
+    it('should return the stemmed word', () => {
+      const originalWord = 'flows' 
+      const result = stemWord(originalWord)
+      expect(result).toEqual('flow')
+    })
+    it('should return original word and singular word', () => {
+      const originalWord = 'flows flow' 
+      const result = stemWord(originalWord)
+      expect(result).toEqual('flow flow')
+    })
+    it('should return original word and singular word when sentence contains stem', () => {
+      const originalWord = 'flows flow flower' 
+      const result = stemWord(originalWord)
+      expect(result).toEqual('flow flow flower')
     })
   })
 })
